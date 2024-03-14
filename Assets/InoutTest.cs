ca44a714900f1343886574fdfb4704c86e36882d
Vasilii Manoilov <wmadwand@gmail.com>
2024-03-14 15:25:30 +0100

    Move player to mouse position

---
+++ Assets/InoutTest.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InoutTest : MonoBehaviour
+{
+    public Player player;
+    public float speed = 10;
+
+    private Vector3 position;
+    private float width;
+    private float height;
+
+    private Vector3 pos;
+
+    void Awake()
+    {
+        width = (float)Screen.width / 2.0f;
+        height = (float)Screen.height / 2.0f;
+
+        // Position used for the cube.
+        position = new Vector3(0.0f, 0.0f, 0.0f);
+
+
+        pos = player.transform.position;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            pos.z = 0;
+        }
+
+        player.transform.position = Vector3.Lerp(player.transform.position, pos, speed * Time.deltaTime);
+
+        return;
+
+        // Handle screen touches.
+        if (Input.touchSupported && Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            // Move the cube if the screen has the finger moving.
+            if (touch.phase == TouchPhase.Moved)
+            {
+                Vector2 pos = touch.position;
+                pos.x = (pos.x - width) / width;
+                pos.y = (pos.y - height) / height;
+                position = new Vector3(-pos.x, pos.y, 0.0f);
+
+                // Position the cube.
+                player.transform.position = position;
+            }
+
+            if (Input.touchCount == 2)
+            {
+                touch = Input.GetTouch(1);
+
+                if (touch.phase == TouchPhase.Began)
+                {
+                    // Halve the size of the cube.
+                    transform.localScale = new Vector3(0.75f, 0.75f, 0.75f);
+                }
+
+                if (touch.phase == TouchPhase.Ended)
+                {
+                    // Restore the regular size of the cube.
+                    transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+                }
+            }
+        }
+    }
+}
