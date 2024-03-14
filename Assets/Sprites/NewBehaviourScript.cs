84a585460f1a98f5dd02c5ea3da0af8750cdbabf
Vasilii Manoilov <wmadwand@gmail.com>
2024-03-14 14:36:39 +0100

    DOTween

--- Assets/Sprites/NewBehaviourScript.cs
+++ Assets/Sprites/NewBehaviourScript.cs
@@ -1,4 +1,5 @@
 using Cysharp.Threading.Tasks;
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,11 +8,12 @@ public class NewBehaviourScript : MonoBehaviour
 {
     private void Start()
     {
-        
+
     }
 
     private async UniTask Test()
     {
         await UniTask.WaitForSeconds(1);
+        var seq = DOTween.Sequence();
     }
 }
